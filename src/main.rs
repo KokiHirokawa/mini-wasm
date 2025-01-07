1a093e452bf3c640946d5b58a75c6550971e8ed1
Koki Hirokawa <pihero@KokinoMacBook-Air.local>
2025-01-08 01:13:22 +0900

    read a wasm file and print a buffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("./empty.wasm")?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
+
+    println!("{:?}", buffer);
+
+    Ok(())
 }
